9b8df43d1c2030a2cc84b9ff10bfb0302a4cd3d3
Pietieklipkop <stefanvandyk3@gmail.com>
2026-07-02 03:48:02 +0200

    CI Tests Fix 6

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 	webServer: { command: 'vp run preview', port: 4173 },
+	use: { baseURL: 'http://localhost:4173' },
 	testMatch: '**/*.e2e.{ts,js}'
 });
